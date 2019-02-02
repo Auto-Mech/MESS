34e72bf1167a904be45dc10fc54e5687c3d22b6a
avcopan <avcopan@gmail.com>
2019-02-02 14:35:19 -0600

    Cleans up build; SLATEC is now a separate conda package

--- build.sh
+++ build.sh
@@ -3,13 +3,9 @@
 mkdir build
 cd build
 
-if [ "$(uname)" == "Darwin" ]; then
-    cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_Fortran_COMPILER=$GFORTRAN -DBLAS_LIBRARIES=$PREFIX/lib/libopenblas.dylib -DLAPACK_LIBRARIES=$PREFIX/lib/libopenblas.dylib
-elif [ "$(uname)" == "Linux" ]; then
-    cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_Fortran_COMPILER=$GFORTRAN -DBLAS_LIBRARIES=$PREFIX/lib/libopenblas.so -DLAPACK_LIBRARIES=$PREFIX/lib/libopenblas.so
-fi
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX
 
-make
+make VERBOSE=1
 make install
 ln -s $PREFIX/bin/messpf $PREFIX/bin/partition_function
 ln -s $PREFIX/bin/messabs $PREFIX/bin/abstraction
---
+++ debug/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PROJECT_ROOT=$THIS_DIR/..
+
+export CXX=$CONDA_PREFIX/bin/x86_64-conda_cos6-linux-gnu-c++
+
+export CXXFLAGS="-fvisibility-inlines-hidden -std=c++17 -fmessage-length=0 -march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-all -fno-plt -Og -g -Wall -Wextra -fvar-tracking-assignments -ffunction-sections -pipe -I${CONDA_PREFIX}/include"
+
+mkdir -p $THIS_DIR/build
+cd $THIS_DIR/build
+
+cmake $PROJECT_ROOT -DCMAKE_INSTALL_PREFIX=$THIS_DIR -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CXX_FLAGS="${CXX_FLAGS}"
+make VERBOSE=1
+make install
