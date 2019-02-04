8dd32497f8ad6cbd53a1f22d427855fdf247b351
avcopan <avcopan@gmail.com>
2019-02-04 10:15:44 -0600

    Updates install instructions

---
+++ debug/install-conda.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $CONDA
+elif [ "$(uname)" == "Linux" ]; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA
+fi
+conda update conda
